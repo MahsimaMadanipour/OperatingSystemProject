c3f72acfca7a8cd4239cc6765f3ea28895d06433
Mahsima Madanipour <91141281+MahsimaMadanipour@users.noreply.github.com>
2022-03-09 23:14:38 +0330

    Delete Sleeping Barber Problem directory

--- Sleeping Barber Problem/costumer.py
+++
@@ -1,6 +0,0 @@
-class Costumer():
-    def __init__(self, name, arrival_t, service_t):
-        self.name = name
-        self.arrival_time = arrival_t
-        self.service_time = service_t
-        self.waiting_time = 0
